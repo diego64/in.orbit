b764b98e4a0c66508793b022b065c1a725d052b7
Diego Ferreira <diegoferreira1964@gmail.com>
2024-10-19 10:55:24 -0300

    feat: swagger setup installation

--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
@@ -8,15 +9,31 @@ import { createGoalRoute } from './routes/create-goal'
 import { createCompletionRoute } from './routes/create-goal-completion'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
-import fastifyCors from '@fastify/cors'
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "in.orbit",
+      version: "1.0.0"
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
 app.register(fastifyCors, {
   origin: '*',
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
 })
 
 app.register(createGoalRoute)
