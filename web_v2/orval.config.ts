4d5a570feb4d07747fb742175b539a78f0253554
Diego Ferreira <diegoferreira1964@gmail.com>
2024-10-31 15:34:45 -0300

    feat: using token in requests

--- web_v2/orval.config.ts
+++ web_v2/orval.config.ts
@@ -2,13 +2,24 @@ import { defineConfig } from 'orval'
 
 export default defineConfig({
   api: {
-    input: '../api/swagger.json',
+    input: '../nlw-pocket-js-node/swagger.json',
     output: {
       baseUrl: 'http://localhost:3333',
       target: './src/http/generated/api.ts',
       client: 'react-query',
       httpClient: 'fetch',
-      clean: true
-    }
-  }
+      clean: true,
+
+      override: {
+        fetch: {
+          includeHttpStatusReturnType: false,
+        },
+
+        mutator: {
+          path: './src/http/client.ts',
+          name: 'http',
+        },
+      },
+    },
+  },
 })
